2cd028f150a25200292f5ad6737982bc407629ff
Ayoub_Jabiri <ayoubjabiri.coding@gmail.com>
2025-09-24 19:02:42 +0200

    first Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const prompt = require("prompt-sync")();
+
+// // Challenge 1
+
+// let arr = [];
+
+// for (let i = 1; i <= 5; i++) {
+//   arr.unshift(prompt(`Donner entier ${i}: `));
+// }
+
+// console.log(arr);
+
+// // Challenge 2
+
+// let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let arr1Som = arr1.reduce((p, c) => {
+//   return p + c;
+// }, 0);
+
+// console.log(
+//   `la somme de tableue est ${arr1Som} et la moyenne est : ${
+//     arr1Som / arr1.length
+//   }`
+// );
